c1eccbf6a959cf398b8e9548fcaf10972e95a93d
SyukriHaziq <97055134+SyukriHaziq@users.noreply.github.com>
2022-11-22 15:57:34 +0800

    Add files via upload

---
+++ ShapeV1/Shape/Shape/Square.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    class Square : Rectangle
+    {
+        public new double Width
+        {
+            get { return base.Width; }
+            set { base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public new double Height
+        {
+            get { return base.Height; }
+            set { base.Height = value;
+                base.Width = value;
+            }
+        }
+
+        public Square() : base()
+        {
+        }
+
+        public double len
+        {
+            get { return base.Height; }
+            set { base.Height = value;
+                base.Width = value;
+            }
+        }
+        public Square(double len) : base(len, len)
+        { 
+        }
+
+    }
+}
